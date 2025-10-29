51973a6581de0728b0afe8162dc6efb1c8bca04c
Gu <gu03fernandes.s@gmail.com>
2025-10-29 17:10:17 -0300

    :recycle: feat: add production origin to CORS middleware

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ origins = [
     "http://localhost:3001",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
+    "https://prospeccao-clientes-iota.vercel.app/prospecting",
 ]
 
 app.add_middleware(
