9e25d7ce67a95f4bbe126fd943bc9f448b3437f5
Gu <gu03fernandes.s@gmail.com>
2025-11-05 14:03:59 -0300

    :recycle:

--- app/schemas/customer.py
+++ app/schemas/customer.py
@@ -1,23 +1,31 @@
-from pydantic import BaseModel, EmailStr, Field
 from datetime import datetime
-from uuid import UUID
 from typing import Optional
+from uuid import UUID
+
+from pydantic import BaseModel, EmailStr, Field
 
 
-class CustomerCreate(BaseModel):
+class CustomerBase(BaseModel):
     company: str = Field(..., min_length=1, max_length=255)
     cnpj: str = Field(..., min_length=1, max_length=32)
     responsible: str = Field(..., min_length=1, max_length=255)
     email: EmailStr
     phone: str = Field(..., min_length=1, max_length=32)
-    main_product: str = Field(..., min_length=1, max_length=255)
-    sku: str = Field(..., min_length=1, max_length=255)
-    supplier: str = Field(..., min_length=1, max_length=255)
+ 
+    main_product: Optional[str] = None
+    sku: Optional[str] = None
+    supplier: Optional[str] = None
 
 
-class CustomerInDB(CustomerCreate):
+class CustomerCreate(CustomerBase):
+     
+    pass
+
+
+class CustomerInDB(CustomerBase):
     id: UUID
     created_at: datetime
+
 
 class CustomerUpdate(BaseModel):
     company: Optional[str] = Field(None, min_length=1, max_length=255)
@@ -25,6 +33,6 @@ class CustomerUpdate(BaseModel):
     responsible: Optional[str] = Field(None, min_length=1, max_length=255)
     email: Optional[EmailStr] = None
     phone: Optional[str] = Field(None, min_length=1, max_length=32)
-    main_product: Optional[str] = Field(None, min_length=1, max_length=255)
-    sku: Optional[str] = Field(None, min_length=1, max_length=255)
-    supplier: Optional[str] = Field(None, min_length=1, max_length=255)
+    main_product: Optional[str] = None
+    sku: Optional[str] = None
+    supplier: Optional[str] = None
